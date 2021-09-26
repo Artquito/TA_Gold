9774280aed748e7da2935b8edf57bd99016b3d37
Cleonart <zredhard@gmail.com>
2021-09-26 17:51:44 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <a-dropdown>
+      <template #overlay>
+        <a-menu @click="handleMenuClick">
+          <a-menu-item key="1">
+            <a href="edit123"><EditOutlined /> Edit</a>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <a href="#"><TrashOutlined /> Edit</a>
+          </a-menu-item>
+        </a-menu>
+      </template>
+      <a-button> Aksi <DownOutlined /></a-button>
+    </a-dropdown>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { DownOutlined, EditOutlined } from "@ant-design/icons-vue";
+export default {
+  setup() {
+    const handleButtonClick = (e) => {
+      console.log("click left button", e);
+    };
+    const handleMenuClick = (e) => {
+      console.log("click", e);
+    };
+    return {
+      handleButtonClick,
+      handleMenuClick,
+    };
+  },
+  components: {
+    DownOutlined,
+    EditOutlined,
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
