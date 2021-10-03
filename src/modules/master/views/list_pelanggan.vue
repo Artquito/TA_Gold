be6772443c834f8fa531797095df5a045cd838fd
Artquito <42540286+Artquito@users.noreply.github.com>
2021-10-03 21:44:58 +0800

    added new elements

---
+++ src/modules/master/views/list_pelanggan.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <a-row justify="end" style="margin:10px 0px">
+            <a-button type="primary">
+                Tambah Pelanggan
+            </a-button>
+        </a-row>
+        <a-table :columns="columns" :dataSource="data">
+
+        </a-table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            data: [
+                {
+                    key:"1",
+                    name:"Art",
+                    id:"1",
+                }
+            ],
+            columns:[
+                {
+                    title: "ID",
+                    dataIndex:"id",
+                    Key: "id",
+                },
+                {
+                    title: "Name",
+                    dataIndex: "name",
+                    Key:"name",
+                },
+            ]
+        }
+    },
+    
+}
+</script>
