05558af57a4d4d931c3d621715803ea6bf90ab2b
Artquito <42540286+Artquito@users.noreply.github.com>
2022-01-09 16:30:43 +0800

    changed majority_chart_config file name

--- src/modules/dashboard/views/dashboard.vue
+++ src/modules/dashboard/views/dashboard.vue
@@ -46,44 +46,6 @@ export default {
   data() {
     return {
       date_range: [],
-      // myChart:[],
-      planetChartData: {
-        type: "line",
-        data: {
-          labels: ["2022-01-03", "2022-01-04", "2022-01-05"],
-          datasets: [
-            {
-              label: "Total Sales",
-              backgroundColor: "rgb(255, 99, 132)",
-              borderColor: "rgb(255, 99, 132)",
-              data: [2, 1, 4],
-            },
-          ],
-        },
-        options: {
-          responsive: true,
-          plugins: {
-            title: {
-              display: true,
-              text: "Bracelet Outbound Historical Data",
-              font: { weight: "bold", size: "14px" },
-            },
-            legend: false,
-          },
-          layout: {
-            padding: 30,
-          },
-          scales: {
-            y: {
-              min: 0,
-              ticks: {
-                stepSize:1,
-                
-              },
-            },
-          },
-        },
-      },
     };
   },
   methods: {
